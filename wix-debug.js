6ecd89b034eae0cbb1ad55c06e67761e9c732f78
boddapQ <156851871+boddapQ@users.noreply.github.com>
2025-03-04 21:22:57 +0000

    Update wix-debug.js

--- wix-debug.js
+++ wix-debug.js
@@ -3,13 +3,21 @@ class CustomIframe extends HTMLElement {
     super();
     const shadow = this.attachShadow({ mode: "open" });
     const iframe = document.createElement("iframe");
-    iframe.src = this.getAttribute("src") || "";
-    iframe.width = this.getAttribute("width") || "100%";
-    iframe.height = this.getAttribute("height") || "100%";
+    iframe.src = "https://app.chaindesk.ai/@zielgruppen";
+
+    console.log(
+      "iframe attributes ==============================>",
+      this.getAttribute("width"),
+      this.getAttribute("height"),
+      this.getAttribute("src")
+    );
+
+    iframe.width = this.getAttribute("width") || "900px";
+    iframe.height = this.getAttribute("height") || "900px";
     iframe.setAttribute("frameborder", "0");
     const style = document.createElement("style");
-    style.textContent = `iframe { display: block; width: 100%; height: 100%; }`;
-    shadow.appendChild(style);
+    // style.textContent = `iframe { display: block; width: 100%; height: 100%; }`;
+    // shadow.appendChild(style);
     shadow.appendChild(iframe);
   }
 }
